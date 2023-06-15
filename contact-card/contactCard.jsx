0e7308a8df2e88b3603cc7fc87dc6396f0d053e8
naa-7 <nalawad000@citymail.cuny.edu>
2023-06-15 13:31:25 -0400

    first push

---
+++ contact-card/contactCard.jsx
@@ -0,0 +1,11 @@
+import React, { Component, ReactPropTypes } from "react";
+
+class ContactCard extends Component {
+    constructor(props) {
+        super(props);
+    }
+
+    render() {
+        return <h1>H</h1>
+    }
+}
